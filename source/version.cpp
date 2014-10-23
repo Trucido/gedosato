52c6be17e2efd4851462266a14fa8ec8a8f72d7a
Trucido <leetminiwheat@gmail.com>
2014-10-23 11:58:29 -0400

    blah

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Tokikagura";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 15;
-const unsigned VER_BUILD = 1623;
+const unsigned VER_BUILD = 1624;
 
-const char* VER_STRING = "version 0.15.1623 built on 2014/10/22 20:16:05";
+const char* VER_STRING = "version 0.15.1624 built on 2014/10/23 11:54:34";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
