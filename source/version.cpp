cf849f692b44a25e38dc4c62d29d2f057dd530eb
Trucido <leetminiwheat@gmail.com>
2014-10-24 13:32:50 -0400

    blah

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Barrage am Ring";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 14;
-const unsigned VER_BUILD = 1603;
+const unsigned VER_BUILD = 1605;
 
-const char* VER_STRING = "version 0.14.1603 built on 2014/10/23 21:55:31";
+const char* VER_STRING = "version 0.14.1605 built on 2014/10/24 13:01:41";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
