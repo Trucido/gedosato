8fa4a8a11e1d9cac02d7156a3b5f5fa97bd21454
Trucido <leetminiwheat@gmail.com>
2014-10-23 20:51:51 -0400

    blah optimizations break stuff i think

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Tokikagura";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 15;
-const unsigned VER_BUILD = 1624;
+const unsigned VER_BUILD = 1625;
 
-const char* VER_STRING = "version 0.15.1624 built on 2014/10/23 11:54:34";
+const char* VER_STRING = "version 0.15.1625 built on 2014/10/23 20:11:59";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
